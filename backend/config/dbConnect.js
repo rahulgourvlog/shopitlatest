a03e6bdc2db44789b3ae0058a8a190688c3b8561
Rahul gour <96679977+rahulgourvlog@users.noreply.github.com>
2024-06-06 12:34:19 +0530

    changesindbconnect

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -1,35 +1,12 @@
 import express from "express";
 import mongoose from "mongoose"
-import dotenv from "dotenv";
 
-dotenv.config();
 export  const connectDB=()=>{
     let DB_URI="";
-    if (process.env.NODE_ENV === 'DEVELOPMENT') {
-        DB_URI = process.env.DB_LOCAL_URI;
-    } else if (process.env.NODE_ENV === "PRODUCTION") {
-        DB_URI = process.env.DB_URI;
-    } else {
-        console.error("NODE_ENV is not set correctly");
-        process.exit(1);
-    }
+    if(process.env.NODE_ENV=='DEVELOPMENT')DB_URI=process.env.DB_LOCAL_URI
+    if(process.env.NODE_ENV=="PRODUCTION")DB_URI=process.env.DB_URI
     
-    if (!DB_URI) {
-        console.error("Database URI is not set");
-        process.exit(1);
-    }
-    
-    mongoose.connect(DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
+    mongoose.connect(DB_URI).then((conn)=>{
+        console.log(`mongodb is sucessfully connected to  host ${conn?.connection?.host}`)
     })
-    .then((conn) => {
-        console.log(`MongoDB successfully connected to host ${conn.connection.host}`);
-    })
-    .catch((err) => {
-        console.error(`Error connecting to MongoDB: ${err.message}`);
-        process.exit(1);
-    });
 }
